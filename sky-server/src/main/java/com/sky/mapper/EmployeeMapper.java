755287ef6e06696cd6d56c5ddf28e7f4fa8ec4e8
xppreston <130163491+xppreston@users.noreply.github.com>
2024-03-21 09:32:14 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -40,4 +40,11 @@ public interface EmployeeMapper {
      */
     void update(Employee employee);
 
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
