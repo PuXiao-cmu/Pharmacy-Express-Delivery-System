877134b58d5caed2a470eec88ef4295e6749d104
xppreston <130163491+xppreston@users.noreply.github.com>
2024-03-23 22:01:01 +0800

    套餐起售停售业务代码开发

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -43,4 +43,11 @@ public interface SetmealService {
      * @param setmealDTO
      */
     void update(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐起售、停售
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
